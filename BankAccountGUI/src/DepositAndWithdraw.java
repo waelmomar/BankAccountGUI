f68e2e1c227ceba971892847fc6a21733b257fea
waelmomar <55214285+waelmomar@users.noreply.github.com>
2019-12-08 20:33:27 -0700

    Add files via upload

---
+++ BankAccountGUI/src/DepositAndWithdraw.java
@@ -0,0 +1,118 @@
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class DepositAndWithdraw extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+	static JTextField textFieldAmount;
+	static JTextField textFieldDescription;
+	private JButton btnConfirm;
+	private JLabel lblAmount;
+	public static BankAccount currentAccount;
+	public static boolean transactionType;
+
+	public DepositAndWithdraw(BankAccount currentAccount, boolean transactionType) {
+
+		setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		setTitle("Deposit");
+		setBounds(100, 100, 300, 230);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		if (!transactionType) {
+			lblAmount = new JLabel("Please provide the amount to be withdrawn:");
+		}
+		else {
+			lblAmount = new JLabel("Please provide the amount to deposit:");
+		}
+		lblAmount.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		lblAmount.setBounds(10, 40, 265, 14);
+		contentPanel.add(lblAmount);
+
+		JLabel lblDescription = new JLabel("(Optional) provide a description");
+		lblDescription.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		lblDescription.setBounds(10, 98, 265, 14);
+		contentPanel.add(lblDescription);
+
+		textFieldAmount = new JTextField();
+		textFieldAmount.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		textFieldAmount.setBounds(10, 67, 256, 20);
+		contentPanel.add(textFieldAmount);
+		textFieldAmount.setColumns(10);
+
+		textFieldDescription = new JTextField();
+		textFieldDescription.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		textFieldDescription.setColumns(10);
+		textFieldDescription.setBounds(10, 123, 256, 20);
+		contentPanel.add(textFieldDescription);
+
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnCancel.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		btnCancel.setBounds(185, 157, 89, 23);
+		contentPanel.add(btnCancel);
+
+		textFieldAmount.getDocument().addDocumentListener(new DocumentListener() {
+			public void changedUpdate(DocumentEvent e) {
+				System.out.println("change");
+				setControls();
+			}
+			public void removeUpdate(DocumentEvent e) {
+				System.out.println("remove");
+				setControls();
+			}
+			public void insertUpdate(DocumentEvent e) {
+				System.out.println("insert");
+				setControls();
+			}
+		});
+
+		btnConfirm = new JButton("OK");
+		btnConfirm.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent arg0) {
+				if (btnConfirm.isEnabled()) {
+					BankAccountGUI.btnDepositAndWithdraw_mouseClicked(arg0);
+				}
+			}
+		});
+
+		btnConfirm.setEnabled(false);
+		btnConfirm.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		btnConfirm.setBounds(86, 157, 89, 23);
+		contentPanel.add(btnConfirm); 
+	}
+
+	private void setControls() {
+		String input = textFieldAmount.getText();
+		try {
+			textFieldDescription.setForeground(Color.BLACK);
+			textFieldAmount.setForeground(Color.BLACK);
+			Double.valueOf(input);
+			this.btnConfirm.setEnabled(true);
+		} catch(Exception s) {
+			textFieldAmount.setForeground(Color.RED);
+			btnConfirm.setEnabled(false);
+		}
+	}
+}
---
+++ BankAccountGUI/src/TransactionDialog.java
@@ -0,0 +1,100 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JCheckBox;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class TransactionDialog extends JDialog {
+
+	private static final long serialVersionUID = 1L;
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textFieldTransactions;
+
+	public static void main(String[] args) {
+		try {
+			TransactionDialog dialog = new TransactionDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public TransactionDialog() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JCheckBox chckbxStartDate = new JCheckBox("Start Date:");
+		chckbxStartDate.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		chckbxStartDate.setBounds(6, 7, 97, 23);
+		contentPanel.add(chckbxStartDate);
+		
+		JCheckBox chckbxEndDate = new JCheckBox("End Date:");
+		chckbxEndDate.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		chckbxEndDate.setBounds(6, 44, 97, 23);
+		contentPanel.add(chckbxEndDate);
+		
+		JComboBox comboBoxStartMonth = new JComboBox();
+		comboBoxStartMonth.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		comboBoxStartMonth.setMaximumRowCount(12);
+		comboBoxStartMonth.setBounds(121, 7, 60, 22);
+		contentPanel.add(comboBoxStartMonth);
+		
+		JComboBox comboBoxEndMonth = new JComboBox();
+		comboBoxEndMonth.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		comboBoxEndMonth.setMaximumRowCount(12);
+		comboBoxEndMonth.setBounds(121, 44, 60, 22);
+		contentPanel.add(comboBoxEndMonth);
+		
+		JComboBox comboBoxStartDay = new JComboBox();
+		comboBoxStartDay.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		comboBoxStartDay.setMaximumRowCount(12);
+		comboBoxStartDay.setBounds(191, 7, 60, 22);
+		contentPanel.add(comboBoxStartDay);
+		
+		JComboBox comboBoxEndDay = new JComboBox();
+		comboBoxEndDay.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		comboBoxEndDay.setMaximumRowCount(12);
+		comboBoxEndDay.setBounds(191, 44, 60, 22);
+		contentPanel.add(comboBoxEndDay);
+		
+		JComboBox comboBoxStartYear = new JComboBox();
+		comboBoxStartYear.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		comboBoxStartYear.setMaximumRowCount(12);
+		comboBoxStartYear.setBounds(261, 7, 71, 22);
+		contentPanel.add(comboBoxStartYear);
+		
+		JComboBox comboBoxEndYear = new JComboBox();
+		comboBoxEndYear.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		comboBoxEndYear.setMaximumRowCount(12);
+		comboBoxEndYear.setBounds(261, 44, 71, 22);
+		contentPanel.add(comboBoxEndYear);
+		
+		JLabel lblLegend = new JLabel("              Date                      Time                           Amount                   Description");
+		lblLegend.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblLegend.setBounds(6, 77, 418, 23);
+		contentPanel.add(lblLegend);
+		
+		JLabel lblLegend2 = new JLabel("--------------------------------------------------------------------------------------------------------");
+		lblLegend2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		lblLegend2.setBounds(6, 97, 418, 14);
+		contentPanel.add(lblLegend2);
+		
+		textFieldTransactions = new JTextField();
+		textFieldTransactions.setEditable(false);
+		textFieldTransactions.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		textFieldTransactions.setBounds(6, 111, 418, 139);
+		contentPanel.add(textFieldTransactions);
+		textFieldTransactions.setColumns(10);
+	}
+}
